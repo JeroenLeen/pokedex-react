c9659bdc38e68bd92bfdac8b43dbe2d5222677f6
Jeroen Leen <leen.jeroen1988@gmail.com>
2023-12-26 09:39:26 +0100

    first version

--- src/PokedexEntry.jsx
+++ src/PokedexEntry.jsx
@@ -1,21 +1,25 @@
+import './PokedexEntry.css'
+
 export default function PokedexEntry(  props) {
-  debugger;
+  
         const imageUrl2 = new URL(
         "/pokemon/Normal/" + props.pokedexEntryNumber + ".png",
         import.meta.url
       ).href;
 
-      debugger;
+      
     return (
       <>
-      <div class="image-container">
-      <img src={imageUrl2} alt="Image" class="image" />
-      <div class="numbers">
-        <p><b>{ props.name }</b></p>
-        <p>Normal: { props.normalNumber }</p>
-        <p>Shiny: {props.shinyNumber }</p>
+      <div className="image-container">
+      <img src={imageUrl2} alt="Image" className="image" />
       </div>
-    </div>
+      <div className="numbers">
+       <div> <b>{ props.name }</b></div>
+       <div className='number-container'><div> Normal: { props.normalNumber }</div>
+        <div>Shiny: {props.shinyNumber }</div></div>
+       
+      </div>
+    
     </>
     )
   }
