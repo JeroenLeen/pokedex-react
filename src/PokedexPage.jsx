a2cc92666d8f7e8a7fe8ef992ee488adbe1deb99
Jeroen Leen <leen.jeroen1988@gmail.com>
2024-01-03 13:27:02 +0100

    sort added

--- src/PokedexPage.jsx
+++ src/PokedexPage.jsx
@@ -16,24 +16,20 @@ export default function PokedexPage() {
   //const items2 = useRef([]);
 
   const [items2, setItems2] = useState([]);
+  const [pokemonsOriginalSort, setPokemonsOriginalSort] = useState([]);
   const [users, setUsers] = useState([]);
   const [usersCalled, setUsersCalled] = useState(false);
   const [usersName, setUserName] = useState(false);
+  const [hasData, setHasData] = useState(true);
   const [pokemonCaugth, setPokemonCaugth] = useState(0);
   const [uniquePokemonCaugth, setUniquePokemonCaugth] = useState(0);
   const [totalShinyCaugth, setTotalShinyCaugth] = useState(0);
+  const sortOptions = [{value:"Pokedex", label:"Pokedex"},{value:"Name", label:"Name"},{value:"Number caught", label:"Number caught"},{value:"Shiny's caught", label:"Shiny's caught"}]
+  const [selectValue,setSelectValue] = useState({value:"Pokedex", label:"Pokedex"});
 
   const routeChange = () =>{ 
     let path = `doubleFinder`; 
     navigate(path);
-  }
-
-  function refreshData() {
-    (async () => {
-        console.log("finding pokemon for:" + usersName);
-        const data  = await resource.getUniquePokedexEntries(usersName)
-        setItems2(data) ;
-  })()
   }
 
   useEffect(() => {
@@ -48,9 +44,10 @@ export default function PokedexPage() {
 
 
   const onChangeHandler = (change) => {
-  
+    setSelectValue({value:"Pokedex", label:"Pokedex"});
     setUserName(change.value);
     (async () => {
+      setHasData(false);
       console.log("finding pokemon for:" + change.value);
       const data  = await resource.getUniquePokedexEntries(change.value)
 
@@ -64,11 +61,57 @@ export default function PokedexPage() {
      
       setPokemonCaugth(totalSum);
       setTotalShinyCaugth(shinySum);
-      setUniquePokemonCaugth( data.filter((entry) => entry.normalNumber > 0 | entry.shinyNumber > 0 ).length);
+      setUniquePokemonCaugth( data.filter((entry) => (entry.normalNumber > 0 | entry.shinyNumber > 0) && !entry.isSeasonal  ).length);
       setItems2(data) ;
- 
+      setPokemonsOriginalSort(data);
       })()
   };
+
+
+  const onSortChangeHandler = (change) => {
+  
+    setSelectValue(change);
+    let tempData = pokemonsOriginalSort.map(a => {return {...a}}) ;
+
+    if(change.value == "Name"){
+      tempData.sort(function (a, b) {
+        if (a.monName < b.monName) {
+          return -1;
+        }
+        if (a.monName > b.monName) {
+          return 1;
+        }
+        return 0;
+      });
+    }
+    if(change.value == "Number caught"){
+      tempData.sort(function (a, b) {
+        if (a.normalNumber > b.normalNumber) {
+          return -1;
+        }
+        if (a.normalNumber < b.normalNumber) {
+          return 1;
+        }
+        return 0;
+      });
+    }
+
+    if(change.value == "Shiny's caught"){
+      tempData.sort(function (a, b) {
+        if (a.shinyNumber > b.shinyNumber) {
+          return -1;
+        }
+        if (a.shinyNumber < b.shinyNumber) {
+          return 1;
+        }
+        return 0;
+      });
+    }
+   
+
+    setItems2(tempData) ;
+  };
+
   return (
 <div className="wholeSite">
     <div className="content">
@@ -110,6 +153,10 @@ export default function PokedexPage() {
     </div>
 
     </div>
+    <div className='sortContainer'>
+      <div className='sortSelectContainer' ><h4 className='selectTitle'>Sort:</h4> <Select isDisabled={hasData} className='sortSelect'  options={sortOptions} onChange={onSortChangeHandler}  value={selectValue}></Select>
+      </div>
+      </div>
      <div className='entries'> 
           {
          items2.map(el =>  <div key={el.key} className="entry">
